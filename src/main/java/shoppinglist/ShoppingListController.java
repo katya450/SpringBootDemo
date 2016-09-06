8f9416153058badbc315796810a2fcaaf990bd04
Katja Lusma <katja.lusma@gmail.com>
2016-09-06 11:26:02 +0300

    backend for items into db

--- src/main/java/shoppinglist/ShoppingListController.java
+++ src/main/java/shoppinglist/ShoppingListController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
