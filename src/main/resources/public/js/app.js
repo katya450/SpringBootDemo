677d9fb3aa620e12f22a80bd0438bc942854c66e
Katja Lusma <katja.lusma@gmail.com>
2016-09-16 17:33:08 +0300

    extracted html row generating into a function

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -13,15 +13,13 @@ $(function() {
 			type: "POST",
 			dataType: "json"
 		})
-		.done(function(response) {	
-			console.log(response);
-	        $("tbody").append('<tr><td><button class="delete" data-id="' + response.id + '">X</button></td><td class="list">' + response.name + '</td><td class="list">' + response.amount + '</td></tr>');
+		.done(function(item) {	
+	        $("tbody").append(shoppingListRow(item));
 		})
 		.fail(function(error) {
 			console.log(error); //TODO: error objectista tsekkaa ja tee erilaisia käsittelyjä eri erroreille
 		});
 	});
-	
 	
 	//sivun latautuessa haetaan kaikki tietokannassa olevat asiat näkyviin
 	$.ajax({
@@ -32,11 +30,15 @@ $(function() {
 	})
 	.done(function(itemlist) {
 		itemlist.forEach(function(item) {
-			$("tbody").append('<tr><td><button class="delete" data-id="' + item.id + '">X</button></td><td class="list">' + item.name + 
-					'</td><td  class="list">' + item.amount + '</td></tr>');
+			$("tbody").append(shoppingListRow(item));
 		})
 	});
 	
+	function shoppingListRow (item) {
+		return '<tr><td><button class="delete" data-id="' + item.id + '">X</button></td><td class="list">' + item.name + 
+		'</td><td  class="list">' + item.amount + '</td></tr>'
+	} 
+		
 	//poistetaan tietyn id:n omaava asia tietokannasta
 	$("tbody").on("click", ".delete", function(event) {	
 		var dataid = $(event.target).attr("data-id");
@@ -48,28 +50,5 @@ $(function() {
 		.done(function(id) {
 			$(".delete[data-id=" + id + "]").closest("tr").remove();
 		});
-		
 	});
-	
-
-	
-	
-		
-	
-	
-	/*$(".delete").click(function(){
-		var dataid = id;
-		console.log ('delete button clicked with id' + dataid);
-
-			
-		$.ajax({
-			url: "/shoppinglist/id=" + dataid,
-			type: "DELETE"
-		})
-		.done(function(itemlist) {
-			itemlist.remove(dataid);
-		});	
-	});
-	
-	*/
 });
